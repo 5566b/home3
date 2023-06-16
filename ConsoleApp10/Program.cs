28b060bde8c0c204b11717fff4142f74e0b099c7
5566b <106473242+5566b@users.noreply.github.com>
2023-06-16 22:05:10 +0300

    Add files via upload

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("enter ");
+var num1 = (Console.ReadLine());
+
+
+Console.WriteLine("Выберите операцию (+, -, *, /:");
+string operation = Console.ReadLine();
+
+
+Console.WriteLine("Введите второе число:");
+double num2 = double.Parse(Console.ReadLine());
+
+double result = 0;
+
+switch (operation)
+{
+    case "+":
+        result = num1 + num2;
+        break;
+    case "-":
+        result = num1 - num2;
+        break;
+    case "*":
+        result = num1 * num2;
+        break;
+    case "/":
+        result = num1 / num2;
+        break;
+    default:
+        Console.WriteLine("Операция не распознана.");
+        break;
+}
+
+Console.WriteLine("Результат: " + result);
