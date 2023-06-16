978bdf24e80166c25d877c0b1951ac4da662b944
5566b <106473242+5566b@users.noreply.github.com>
2023-06-16 22:36:20 +0300

    Add files via upload

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -1,33 +1,41 @@
-﻿Console.WriteLine("enter ");
-var num1 = (Console.ReadLine());
-
-
-Console.WriteLine("Выберите операцию (+, -, *, /:");
-string operation = Console.ReadLine();
-
-
-Console.WriteLine("Введите второе число:");
-double num2 = double.Parse(Console.ReadLine());
-
-double result = 0;
-
-switch (operation)
+﻿
+class Program
 {
-    case "+":
-        result = num1 + num2;
-        break;
-    case "-":
-        result = num1 - num2;
-        break;
-    case "*":
-        result = num1 * num2;
-        break;
-    case "/":
-        result = num1 / num2;
-        break;
-    default:
-        Console.WriteLine("Операция не распознана.");
-        break;
-}
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        double num1 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Результат: " + result);
+      
+        Console.WriteLine("Выберите операцию (+, -, *, /):");
+        string operation = Console.ReadLine();
+
+        Console.WriteLine("Введите второе число:");
+        double num2 = double.Parse(Console.ReadLine());
+
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Операция не распознана.");
+                break;
+        }
+
+        Console.WriteLine("Результат: " + result);
+
+    }
+}
